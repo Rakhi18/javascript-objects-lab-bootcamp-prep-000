318912e48618e184bdb3abf4a5616fcad4631d9b
rakhi dawande <rakhi1997dawande@gmail.com>
2019-06-12 06:17:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ function updateObjectWithKeyAndValue(object, key, value) {
   return clone
 }
 function destructvelyUpdateObjectWithKeyAndValue(object,key,value) {
-  return object.assign(object, {[key]: value})
+  object[key] = value
+  return object
 }
 function deleteFromObjectByKey(object, key) {
   
